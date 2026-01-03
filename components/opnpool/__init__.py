a704a9a0e085ce739f1c44b07260bb66fe4a2639
Coert Vonk <coert.vonk@gmail.com>
2026-01-02 22:21:31 -0800

    add device class to analog sensors

--- components/opnpool/__init__.py
+++ components/opnpool/__init__.py
@@ -2,31 +2,39 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, switch, sensor, binary_sensor, text_sensor, climate
 from esphome.const import (
-    CONF_ID, CONF_UART_ID, UNIT_WATT, UNIT_PERCENT, STATE_CLASS_MEASUREMENT
+    CONF_ID, CONF_UART_ID, CONF_UNIT_OF_MEASUREMENT, CONF_DEVICE_CLASS, UNIT_WATT, UNIT_PERCENT, STATE_CLASS_MEASUREMENT
 )
 
 DEPENDENCIES = ["switch", "sensor", "binary_sensor", "text_sensor", "climate"]
 AUTO_LOAD = ["switch", "sensor", "binary_sensor", "text_sensor", "climate"]
 
-# Namespace and Class Definitions
+# namespace and class definitions
 opnpool_ns = cg.esphome_ns.namespace("opnpool")
 OpnPool = opnpool_ns.class_("OpnPool", cg.Component)
 OpnPoolSwitch = opnpool_ns.class_("OpnPoolSwitch", switch.Switch)
 OpnPoolClimate = opnpool_ns.class_("OpnPoolClimate", climate.Climate)
 
-# Entity Definitions
-CONF_SWITCHES = ["pool", "spa", "aux1", "aux2", "flt1", "flt2", "flt3", "flt4"]
+# entity definitions
+CONF_HEATERS = ["pool_heater", "spa_heater"]
+CONF_SWITCHES = ["pool", "spa", "aux1", "aux2", "feature1", "feature2", "feature3", "feature4"]
 CONF_TEXT_SENSORS = [
     "pool_sched", "spa_sched", "aux1_sched", "aux2_sched", 
     "system_time", "controller_firmware_version", "interface_firmware_version",
     "pump_mode", "chlorinator_name"
 ]
-CONF_ANALOG_SENSORS = {
-    "air_temperature": "°C", "water_temperature": "°C",
-    "pump_power": UNIT_WATT, "pump_flow": "gpm", "pump_speed": "rpm",
-    "chlorinator_percentage": UNIT_PERCENT, "chlorinator_salt": "ppm",
-    "pump_status": "", "pump_state": "", "pump_error": "", "chlorinator_status": ""
-}
+CONF_ANALOG_SENSORS = [
+    "air_temperature",
+    "water_temperature",
+    "pump_power",
+    "pump_flow",
+    "pump_speed",
+    "chlorinator_percentage",
+    "chlorinator_salt",
+    "pump_status",
+    "pump_state",
+    "pump_error",
+    "chlorinator_status"
+]
 CONF_BINARY_SENSORS = [
     "pump_running", "mode_service", "mode_temperature_inc", 
     "mode_freeze_protection", "mode_timeout"
@@ -35,23 +43,24 @@ CONF_BINARY_SENSORS = [
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(OpnPool),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Optional("pool_heater"): climate.climate_schema(OpnPoolClimate),
-    cv.Optional("spa_heater"): climate.climate_schema(OpnPoolClimate),
+
+    **{cv.Optional(h): climate.climate_schema(OpnPoolClimate) for h in CONF_HEATERS},
     **{cv.Optional(s): switch.switch_schema(OpnPoolSwitch) for s in CONF_SWITCHES},
     **{cv.Optional(ts): text_sensor.text_sensor_schema() for ts in CONF_TEXT_SENSORS},
-    **{cv.Optional(as_): sensor.sensor_schema(unit_of_measurement=u, state_class=STATE_CLASS_MEASUREMENT) 
-       for as_, u in CONF_ANALOG_SENSORS.items()},
+    **{cv.Optional(sensor_key): sensor.sensor_schema() for sensor_key in CONF_ANALOG_SENSORS},
     **{cv.Optional(bs): binary_sensor.binary_sensor_schema() for bs in CONF_BINARY_SENSORS},
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    
+    # instantiate the main OpnPool component
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # link the C++ class to the UART-bus and handle the direction pin
+    # link the C++ class to the UART-bus
     await uart.register_uart_device(var, config)
 
-    # Climate Registration
+    # climate registration
     for heater in ["pool_heater", "spa_heater"]:
         if heater in config:
             clm = cg.new_Pvariable(config[heater][CONF_ID])
@@ -64,11 +73,12 @@ async def to_code(config):
             await switch.register_switch(sw, config[key])
             cg.add(getattr(var, f"set_{key}_switch")(sw))
 
-    for key in CONF_ANALOG_SENSORS:
-        if key in config:
-            sens = await sensor.new_sensor(config[key])
-            cg.add(getattr(var, f"set_{key}_sensor")(sens))
-
+    for sensor_key in CONF_ANALOG_SENSORS:
+        if sensor_key in config:
+            # Create the sensor object with all YAML settings (device_class, units, etc.)
+            sens = await sensor.new_sensor(config[sensor_key])
+            # Call the corresponding C++ setter: e.g., var->set_water_temperature_sensor(sens)
+            cg.add(getattr(var, f"set_{sensor_key}_sensor")(sens))
     for key in CONF_TEXT_SENSORS:
         if key in config:
             tsens = await text_sensor.new_text_sensor(config[key])
