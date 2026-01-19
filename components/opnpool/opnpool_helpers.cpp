42024ba8cb21874d03f8c01e3f6289e6d0752a96
Coert Vonk <coert.vonk@gmail.com>
2026-01-19 00:44:08 -0800

    more sanity checks

--- components/opnpool/opnpool_helpers.cpp
+++ components/opnpool/opnpool_helpers.cpp
@@ -24,6 +24,9 @@ poolstate_thermo_typ_t
 climate_id_to_poolstate_thermo(ClimateId const id)
 {
     static_assert(enum_count<ClimateId>() == enum_count<poolstate_thermo_typ_t>(), "ClimateId and poolstate_thermo_typ_t must have the same number of elements");
+    static_assert(enum_index(ClimateId::SPA_CLIMATE) == enum_index(poolstate_thermo_typ_t::SPA), "ClimateId and poolstate_thermo_typ_t must have matching elements");
+    static_assert(enum_index(ClimateId::POOL_CLIMATE) == enum_index(poolstate_thermo_typ_t::POOL), "ClimateId and poolstate_thermo_typ_t must have matching elements");
+
     return static_cast<poolstate_thermo_typ_t>(static_cast<uint8_t>(id));
 }
 
@@ -42,6 +45,16 @@ network_pool_circuit_t
 switch_id_to_network_circuit(SwitchId const id)
 {
     static_assert(enum_count<SwitchId>() == enum_count<network_pool_circuit_t>(), "SwitchId and network_pool_circuit_t must have the same number of elements");
+    static_assert(enum_index(SwitchId::SPA) == enum_index(network_pool_circuit_t::SPA), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::POOL) == enum_index(network_pool_circuit_t::POOL), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::AUX1) == enum_index(network_pool_circuit_t::AUX1), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::AUX2) == enum_index(network_pool_circuit_t::AUX2), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::AUX3) == enum_index(network_pool_circuit_t::AUX3), "SwitchId and network_pool_circuit_t must have matching elements"); 
+    static_assert(enum_index(SwitchId::FEATURE1) == enum_index(network_pool_circuit_t::FEATURE1), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::FEATURE2) == enum_index(network_pool_circuit_t::FEATURE2), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::FEATURE3) == enum_index(network_pool_circuit_t::FEATURE3), "SwitchId and network_pool_circuit_t must have matching elements");
+    static_assert(enum_index(SwitchId::FEATURE4) == enum_index(network_pool_circuit_t::FEATURE4), "SwitchId and network_pool_circuit_t must have matching elements");
+
     return static_cast<network_pool_circuit_t>(static_cast<uint8_t>(id));
 }
 
